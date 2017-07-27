c36543316d3ba2d29a5fe120b8e29a23d7780e67
huangwei <huangwei@zhongan.com>
2017-07-27 11:29:35 +0800

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='tornado-redis-session',
+    packages=['tornado_redis_session'],
+    version='0.1.0',
+    description='Server side session middleware based on redis',
+    author='David Wong',
+    author_email='stef-hw@163.com',
+    url='https://github.com/hw20686832/tornado-redis-session',
+    download_url='https://github.com/hw20686832/tornado-redis-session/archive/0.1.0.tar.gz',
+    keywords=['tornado', 'session', 'redis'],
+    install_requires=['M2Crypto', 'redis', 'tornado'],
+    classifiers=[],
+)
