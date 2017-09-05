f89803d1c85d6241936a313ad92920846216aa9b
huangwei <huangwei@zhongan.com>
2017-09-05 18:09:42 +0800

    remove M2Crypto

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 setup(
     name='tornado-redis-session',
     packages=['tornado_redis_session'],
-    version='0.1.0',
+    version='0.1.1',
     description='Server side session middleware based on redis',
     author='David Wong',
     author_email='stef-hw@163.com',
     url='https://github.com/hw20686832/tornado-redis-session',
-    download_url='https://github.com/hw20686832/tornado-redis-session/archive/0.1.0.tar.gz',
+    download_url='https://github.com/hw20686832/tornado-redis-session/archive/0.1.1.tar.gz',
     keywords=['tornado', 'session', 'redis'],
-    install_requires=['M2Crypto', 'redis', 'tornado'],
+    install_requires=['redis', 'tornado'],
     classifiers=[],
 )
